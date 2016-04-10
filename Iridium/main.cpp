21ebc4247c5ed3718ade1a079214596cad2d39bc
Kian Shepherd <kianshepherd29@gmail.com>
2016-04-10 15:06:57 -0400

    PLEASE FOR ONCE WORK

--- Iridium/main.cpp
+++ Iridium/main.cpp
@@ -7,6 +7,13 @@ int main()
 {
     sf::RenderWindow win(sf::VideoMode(1080, 720), "Iridium");
     win.setFramerateLimit(60);
+    win.setKeyRepeatEnabled(false);
+
+    bool keyState[sf::Keyboard::KeyCount];
+    for (int i = 0; i < (sf::Keyboard::KeyCount); i++)
+    {
+        keyState[i] = true;
+    }
 
     int corb1_x, corb1_y, corb2_x, corb2_y;
     corb1_y = corb2_y = 30;
@@ -37,9 +44,10 @@ int main()
     pushpull = -1;                          // pushpull is 1 when pushpulling, -1 when repelling
     sf::Event event;
     win.setKeyRepeatEnabled(false);
-
     while (win.isOpen())
     {
+
+
         win.clear();
         lines[0].position = sf::Vector2f(0, 0);
         lines[1].position = sf::Vector2f(0, 0);
@@ -48,7 +56,7 @@ int main()
         x_speed *= 0.995f;
         y_speed *= 0.98f;
         y_speed += gravity;
-
+        std::cout << pushpull << std::endl;
         while (win.pollEvent(event))
             {
                 switch (event.type)
@@ -56,18 +64,19 @@ int main()
                 case sf::Event::Closed:
                     win.close();
                     break;
-
                 case sf::Event::KeyPressed:
                     if (event.key.code == sf::Keyboard::Escape) {win.close();}
-                    if (event.key.code == sf::Keyboard::Space) {pushpull *= -1; std::cout << "Space" << std::endl;}
+                    if (event.key.code == sf::Keyboard::Space && !keyState[event.key.code]) {pushpull *= -1;}
+                    keyState[event.key.code] = true;
                     break;
-
+                case sf::Event::KeyReleased:
+                    keyState[event.key.code] = false;
+                    break;
                 default:
                     break;
                 }
             }
 
-        if (event.key.code == sf::Keyboard::Space){pushpull *= -1;}
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
             if (corb1_x >= x){x_speed += (pull_strength * pushpull);}
@@ -91,9 +100,9 @@ int main()
         x += x_speed;
         if (y > (720 - 52)){on_ground = true; y_speed = 0;} else {on_ground = false;}
         if (y > (720 - 50)){y = 720 - 50;}
-        if (x > (corb2_x - 5)){x = (corb2_x - 6);}
-        if (x < (corb1_x + 5)){x = (corb1_x + 6);}
-        if (y < (corb1_y + 5)){y = (corb1_y + 6);}
+        if (x > (corb2_x - 5)){x = (corb2_x - 6); x_speed = 0;}
+        if (x < (corb1_x + 5)){x = (corb1_x + 6); x_speed = 0;}
+        if (y < (corb1_y + 5)){y = (corb1_y + 6); y_speed = 0;}
 
         ball.setPosition(x, y);
         win.draw(lines);
