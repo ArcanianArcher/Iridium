f16765fd138b9b5df466526f37f681a85dddfb3f
Kian Shepherd <kianshepherd29@gmail.com>
2016-04-12 00:04:06 -0400

    its late i dont want to fix it

--- Iridium/main.cpp
+++ Iridium/main.cpp
@@ -30,25 +30,31 @@ public:
         std::cout << "new level: " << level_number << std::endl;
     };
 
-    bool IsColliding(int x, int y, int r, int p)
+    int IsColliding(int x, int y, int r, int p)
     {
-        /*
+
         int x_points[p];
         int y_points[p];
-
+        std::cout << "center (x,y): ("<<x<<", "<<y<<")"<<std::endl;
         for (int i = 0; i < p; i++)
         {
             x_points[i] = abs(x + (r * cos(((2 * i) * M_PI) / p)));
             y_points[i] = abs(y + (r * sin(((2 * i) * M_PI) / p)));
+            std::cout << "x, y points[" <<i<<"] (x,y): ("<<x_points[i]<<", "<<y_points[i]<<")"<<std::endl;
         }
-        */
+        std::cout<<std::endl;
+        std::cout<<std::endl;
+        std::cout<<std::endl;
+        std::cout<<std::endl;
+        std::cout<<std::endl;
+
         for (int i = 0; i < p; i++)
         {
             if (level_image.getPixel(abs(x + (r * cos(((2 * i) * M_PI) / p))), abs(y + (r * sin(((2 * i) * M_PI) / p)))) == black)
-                {return true;} //live code
-                //{std::cout<<"colliding"<<std::endl; return true;}else{std::cout<<std::endl;} //testing purposess
+                //{return i;} //live code
+                {std::cout<<"colliding: "<<i<<std::endl; return i;}else{std::cout<<std::endl;} //testing purposess
         }
-        return false;
+        return -1;
     };
     sf::Sprite GetSprite(){return level_sprite;}
 };
@@ -57,22 +63,24 @@ public:
 int main()
 {
     sf::RenderWindow win(sf::VideoMode(1080, 720), "Iridium");
-    win.setFramerateLimit(80);
+    win.setFramerateLimit(5);
     win.setKeyRepeatEnabled(false);
 
     int current_level_num = 1;
     Level* level = new Level(current_level_num);
 
     bool keyState[sf::Keyboard::KeyCount];
-    bool is_collided;
+    int is_collided;
     for (int i = 0; i < (sf::Keyboard::KeyCount); i++){keyState[i] = false;}
     int corb1_x, corb1_y, corb2_x, corb2_y, ball_r, corb_r, detection_points;
+    float friction_constant;
     corb1_y = corb2_y = 30;
     corb1_x = 30;
     corb2_x = 1000;
     ball_r = 25;
     corb_r = 25;
-    detection_points = 6;
+    friction_constant = 0.7f;
+    detection_points = 8;
 
     sf::VertexArray lines(sf::Lines, 4);
 
@@ -105,7 +113,7 @@ int main()
         lines[2].position = sf::Vector2f(0, 0);
         lines[3].position = sf::Vector2f(0, 0);
         y_speed += gravity;
-        is_collided = level->IsColliding(x, y, ball_r, detection_points);
+
         while (win.pollEvent(event))
             {
                 switch (event.type)
@@ -147,7 +155,22 @@ int main()
 
         y += y_speed;
         x += x_speed;
-
+        is_collided = level->IsColliding(x, y, ball_r, detection_points);
+        if (is_collided != -1)
+        {
+            if (is_collided == 0 || is_collided == 4)
+            {
+                x_speed *= -1; x += x_speed; x_speed *= friction_constant; y_speed *= friction_constant;
+            }
+            else if (is_collided == 2 || is_collided == 6)
+            {
+                y_speed *= -1; y += y_speed; x_speed *= friction_constant; y_speed *= friction_constant;
+            }
+            else
+            {
+                x_speed *= -1; x += x_speed; y_speed *= -1; y += y_speed; x_speed *= friction_constant; y_speed *= friction_constant;
+            }
+        }
         if (x > (corb2_x - 5)){x = (corb2_x - 6); x_speed = 0;}
         if (x < (corb1_x + 5)){x = (corb1_x + 6); x_speed = 0;}
         if (y < (corb1_y + 5)){y = (corb1_y + 6); y_speed = 0;}
