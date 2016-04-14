55388a85ae6041e17f8abd57335d7f27d73148cb
Kian Shepherd <kianshepherd29@gmail.com>
2016-04-14 11:18:11 -0400

    Huge change game now separate class

--- Iridium/main.cpp
+++ Iridium/main.cpp
@@ -13,7 +13,19 @@ const double PI  =3.141592653589793238463;
 #define SSTR( x ) static_cast< std::ostringstream & >( \
         ( std::ostringstream() << std::dec << x ) ).str()
 
-
+sf::Vector2f AddVectors(int *points, int total_detection_points, double total_speed)
+{
+    sf::Vector2f vector_total(0.f, 0.f);
+    for (int i = 0; i < points[0]; i++)
+    {
+        float angle = (((2 * points[i + 1]) * PI) / total_detection_points);
+        sf::Vector2f v(-(cos(angle)), -(sin(angle)));
+        vector_total += v;
+    }
+    float length = sqrt((vector_total.x * vector_total.x) + (vector_total.y * vector_total.y));
+    sf::Vector2f u((vector_total.x / length) * total_speed, (vector_total.y / length) * total_speed);
+    return u;
+}
 
 class Level
 {
@@ -77,106 +89,50 @@ public:
     }
 };
 
-
-
-sf::Vector2f AddVectors(int *points, int total_detection_points, double total_speed)
+class Game
 {
-    sf::Vector2f vector_total(0.f, 0.f);
-    for (int i = 0; i < points[0]; i++)
-    {
-        float angle = (((2 * points[i + 1]) * PI) / total_detection_points);
-        sf::Vector2f v(-(cos(angle)), -(sin(angle)));
-        vector_total += v;
-    }
-    float length = sqrt((vector_total.x * vector_total.x) + (vector_total.y * vector_total.y));
-    sf::Vector2f u((vector_total.x / length) * total_speed, (vector_total.y / length) * total_speed);
-    return u;
-}
-
-int main()
-{
-    sf::RenderWindow win(sf::VideoMode(1080, 720), "Iridium");
-    win.setFramerateLimit(80);
-    win.setKeyRepeatEnabled(false);
-
-    int current_level_num = 0;
-    Level* level = new Level(current_level_num, 30, 30, 1000, 30, sf::Color::Red);
-
-    bool keyState[sf::Keyboard::KeyCount];
-    for (int i = 0; i < (sf::Keyboard::KeyCount); i++){keyState[i] = false;}
-    int ball_r;
+public:
+    int current_level_num;
+    int ball_r = 25;
     int Quadrant_m1 = 1;
     int Quadrant_m2 = 1;
-    const int detection_points = 36;
-    int detected_points[detection_points + 1];
-    float i_friction_constant;
-    ball_r = 25;
-    i_friction_constant = 0.3f;
-    sf::VertexArray lines(sf::Lines, 4);
-    sf::CircleShape ball(ball_r);
-    ball.setFillColor(sf::Color::Green);
+    int detection_points = 36;
+    int detected_points[36 + 1];
+    float x, y, prev_x, prev_y, gravity, x_speed, y_speed, pull_strength, pull_constant, i_friction_constant;
+    bool pushpull = true;
+    sf::VertexArray lines;
+    sf::CircleShape ball;
+    Level* level;
 
-    float x, y, prev_x, prev_y, gravity, x_speed, y_speed, pull_strength, pull_constant;
-    bool pushpull;
-    x = 515;
-    y = 100;
-    gravity = 0.1f;
-    x_speed = y_speed = 0.f;
-    pull_constant = 0.0005f;
-    pushpull = true;                          // pushpull is true when pulling, false when repelling
-    sf::Event event;
-    win.setKeyRepeatEnabled(false);
-    while (win.isOpen())
+
+
+    Game()
     {
-        win.clear();
+        current_level_num = 0;
+        x = 515;
+        y = 100;
+        x_speed = y_speed = 0.f;
+        gravity = 0.1f;
+        i_friction_constant = 0.3f;
+        pull_constant = 0.0005f;
+        lines.resize(4);
+        //lines.append(sf::Lines);
+        ball.setRadius(ball_r);
+        ball.setFillColor(sf::Color::Green);
+
+        level = new Level(current_level_num, 30, 30, 1000, 30, sf::Color::Red);
+    }
+
+    void ResetLines()
+    {
         lines[0].position = sf::Vector2f(0, 0); lines[0].color = sf::Color::Blue;
         lines[1].position = sf::Vector2f(0, 0); lines[1].color = sf::Color::Blue;
         lines[2].position = sf::Vector2f(0, 0); lines[2].color = sf::Color::Blue;
         lines[3].position = sf::Vector2f(0, 0); lines[3].color = sf::Color::Blue;
-        y_speed += gravity;
-
-        while (win.pollEvent(event))
-            {
-                switch (event.type)
-                {
-                case sf::Event::Closed:
-                    win.close();
-                    break;
-                case sf::Event::KeyPressed:
-                    if (event.key.code == sf::Keyboard::Escape) {win.close();}
-                    if (event.key.code == sf::Keyboard::Space && !keyState[event.key.code]) {pushpull = !pushpull;}
-                    if (event.key.code == sf::Keyboard::R && !keyState[event.key.code]) {current_level_num++; level = new Level(current_level_num, 30, 30, 1000, 30, sf::Color::Red);}
-                    keyState[event.key.code] = true;
-                    break;
-                case sf::Event::KeyReleased:
-                    keyState[event.key.code] = false;
-                    break;
-                default:
-                    break;
-                }
-            }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            if ((x - level -> GetControlOrb1Position().x > 0) && (y - level -> GetControlOrb1Position().y <= 0)) {Quadrant_m1 = -1; Quadrant_m2 = 1;}       // Q1
-            else if ((x - level -> GetControlOrb1Position().x <= 0) && (y - level -> GetControlOrb1Position().y < 0)) {Quadrant_m1 = 1; Quadrant_m2 = 1;}   // Q2
-            else if ((x - level -> GetControlOrb1Position().x < 0) && (y - level -> GetControlOrb1Position().y >= 0)) {Quadrant_m1 = 1; Quadrant_m2 = -1;}  // Q3
-            else {Quadrant_m1 = -1; Quadrant_m2 = -1;}                                                                                                      // Q4
-            pull_strength = (sqrt(pow(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))), 2) + pow(fabs(((x + level -> control_radius) - (level -> GetControlOrb1Position().x + level -> control_radius))), 2))) * 0.9f;
-            if (pushpull == true)
-            {
-                x_speed += (cos(atan(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))) / fabs(((x + ball_r) - (level -> GetControlOrb1Position().x + level -> control_radius)))))) * Quadrant_m1 * ((pull_strength * pull_constant));
-                y_speed += (sin(atan(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))) / fabs(((x + ball_r) - (level -> GetControlOrb1Position().x + level -> control_radius)))))) * Quadrant_m2 * ((pull_strength * (pull_constant / 0.5f)));
-            }else{
-                x_speed += (cos(atan(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))) / fabs(((x + ball_r) - (level -> GetControlOrb1Position().x + level -> control_radius)))))) * (1 / Quadrant_m1 * ((pull_strength * pull_constant) * -1)) * 0.1f;
-                y_speed += (sin(atan(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))) / fabs(((x + ball_r) - (level -> GetControlOrb1Position().x + level -> control_radius)))))) * (1 / Quadrant_m2 * ((pull_strength * pull_constant / 0.5f) * -1)) * 0.1f;
-            }
-            lines[0].position = sf::Vector2f(level -> GetControlOrb1Position().x + level -> control_radius, level -> GetControlOrb1Position().y + level -> control_radius);
-            lines[1].position = sf::Vector2f((x + ball_r), (y + ball_r));
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            if ((x - level -> GetControlOrb2Position().x > 0) && (y - level -> GetControlOrb2Position().y <= 0)) {Quadrant_m1 = -1; Quadrant_m2 = 1;}       // Q1
+    }
+    void RightOrb()
+    {
+        if ((x - level -> GetControlOrb2Position().x > 0) && (y - level -> GetControlOrb2Position().y <= 0)) {Quadrant_m1 = -1; Quadrant_m2 = 1;}       // Q1
             else if ((x - level -> GetControlOrb2Position().x <= 0) && (y - level -> GetControlOrb2Position().y < 0)) {Quadrant_m1 = 1; Quadrant_m2 = 1;}   // Q2
             else if ((x - level -> GetControlOrb2Position().x < 0) && (y - level -> GetControlOrb2Position().y >= 0)) {Quadrant_m1 = 1; Quadrant_m2 = -1;}  // Q3
             else {Quadrant_m1 = -1; Quadrant_m2 = -1;}                                                                                                      // Q4
@@ -191,8 +147,32 @@ int main()
             }
             lines[0].position = sf::Vector2f(level -> GetControlOrb2Position().x + level -> control_radius, level -> GetControlOrb2Position().y + level -> control_radius);
             lines[1].position = sf::Vector2f((x + ball_r), (y + ball_r));
-        }
-
+    }
+    void LeftOrb()
+    {
+        if ((x - level -> GetControlOrb1Position().x > 0) && (y - level -> GetControlOrb1Position().y <= 0)) {Quadrant_m1 = -1; Quadrant_m2 = 1;}       // Q1
+            else if ((x - level -> GetControlOrb1Position().x <= 0) && (y - level -> GetControlOrb1Position().y < 0)) {Quadrant_m1 = 1; Quadrant_m2 = 1;}   // Q2
+            else if ((x - level -> GetControlOrb1Position().x < 0) && (y - level -> GetControlOrb1Position().y >= 0)) {Quadrant_m1 = 1; Quadrant_m2 = -1;}  // Q3
+            else {Quadrant_m1 = -1; Quadrant_m2 = -1;}                                                                                                      // Q4
+            pull_strength = (sqrt(pow(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))), 2) + pow(fabs(((x + level -> control_radius) - (level -> GetControlOrb1Position().x + level -> control_radius))), 2))) * 0.9f;
+            if (pushpull == true)
+            {
+                x_speed += (cos(atan(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))) / fabs(((x + ball_r) - (level -> GetControlOrb1Position().x + level -> control_radius)))))) * Quadrant_m1 * ((pull_strength * pull_constant));
+                y_speed += (sin(atan(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))) / fabs(((x + ball_r) - (level -> GetControlOrb1Position().x + level -> control_radius)))))) * Quadrant_m2 * ((pull_strength * (pull_constant / 0.5f)));
+            }else{
+                x_speed += (cos(atan(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))) / fabs(((x + ball_r) - (level -> GetControlOrb1Position().x + level -> control_radius)))))) * (1 / Quadrant_m1 * ((pull_strength * pull_constant) * -1)) * 0.1f;
+                y_speed += (sin(atan(fabs(((y + ball_r) - (level -> GetControlOrb1Position().y + level -> control_radius))) / fabs(((x + ball_r) - (level -> GetControlOrb1Position().x + level -> control_radius)))))) * (1 / Quadrant_m2 * ((pull_strength * pull_constant / 0.5f) * -1)) * 0.1f;
+            }
+            lines[0].position = sf::Vector2f(level -> GetControlOrb1Position().x + level -> control_radius, level -> GetControlOrb1Position().y + level -> control_radius);
+            lines[1].position = sf::Vector2f((x + ball_r), (y + ball_r));
+    }
+    void SwitchPushPull()
+    {
+        pushpull = !pushpull;
+    }
+    void Movement()
+    {
+        y_speed += gravity;
         prev_x = x;
         prev_y = y;
         y += y_speed;
@@ -215,11 +195,87 @@ int main()
             level = new Level(current_level_num, 30, 30, 1000, 30, sf::Color::Red);
         }
         ball.setPosition(x, y);
-        win.draw(level->GetSprite());
-        win.draw(lines);
-        win.draw(ball);
-        win.draw(level -> control1);
-        win.draw(level -> control2);
+
+
+    }
+
+};
+
+
+
+int main()
+{
+    sf::RenderWindow win(sf::VideoMode(1080, 720), "Iridium");
+    win.setFramerateLimit(80);
+    win.setKeyRepeatEnabled(false);
+    sf::Event event;
+    bool poll_event;
+    bool keyState[sf::Keyboard::KeyCount];
+    for(int i = 0; i < (sf::Keyboard::KeyCount); i++){keyState[i] = false;}
+    int game_state = 1; //main menu 0, game 1...
+    Game* game = new Game();
+
+    while (win.isOpen())
+    {
+        win.clear();
+        /*
+        poll_event = win.pollEvent(event);
+        while (poll_event)
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                win.close();
+                break;
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::Escape) {win.close();}
+                break;
+            case sf::Event::KeyReleased:
+                break;
+            default:
+                break;
+            }
+        }*/
+        if (game_state == 1)
+        {
+            game->ResetLines();
+            game->Movement();
+            while(win.pollEvent(event))
+            {
+                switch(event.type)
+                {
+                    case sf::Event::Closed:
+                        win.close();
+                        break;
+                    case sf::Event::KeyPressed:
+                        if (event.key.code == sf::Keyboard::Escape) {win.close();}
+                        if (event.key.code == sf::Keyboard::Space && !keyState[event.key.code])
+                            {game->SwitchPushPull();}
+                        //if (event.key.code == sf::Keyboard::R && !keyState[event.key.code]) {current_level_num++; level = new Level(current_level_num, 30, 30, 1000, 30, sf::Color::Red);}
+                        keyState[event.key.code] = true;
+                        break;
+                    case sf::Event::KeyReleased:
+                        keyState[event.key.code] = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                game->LeftOrb();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                game->RightOrb();
+            }
+
+            win.draw(game->level->GetSprite());
+            win.draw(game->lines);
+            win.draw(game->ball);
+            win.draw(game->level -> control1);
+            win.draw(game->level -> control2);
+        }
         win.display();
     }
     return 0;
